04bd1925ea1636e0f8c1800e19b4aa494c57d41a
AlgerDu <AlgerDu@outlook.com>
2023-09-03 11:50:25 +0800

    Update exts.go

--- src/exts.go
+++ src/exts.go
@@ -6,9 +6,26 @@ func Collector_AddInstance[insType any, dstType any](services ServiceCollector, 
 
 	tmpDst := new(dstType)
 
-	return services.AddService(ServiceDescriptor{
-		LifeTime: SL_Singleton,
-		Type:     reflect.TypeOf(ins),
-		DstType:  reflect.TypeOf(tmpDst).Elem(),
+	return services.AddService(&ServiceDescriptor{
+		LifeTime:    SL_Singleton,
+		Type:        reflect.TypeOf(ins),
+		DstType:     reflect.TypeOf(tmpDst).Elem(),
+		Instance:    reflect.ValueOf(ins),
+		hasInstance: true,
 	})
+}
+
+func Collector_AddScope[T any](services ServiceCollector, creator any) error {
+
+	t := new(T)
+
+	return services.AddService(&ServiceDescriptor{
+		LifeTime:    SL_Scoped,
+		Type:        reflect.TypeOf(t),
+		DstType:     nil,
+		Instance:    reflect.Value{},
+		Creator:     reflect.Value{},
+		hasInstance: false,
+	})
+
 }
